7674e6b6a30ed8c5e4b475aa3a1d4f6d3f736545
Sri Gutala <sri.gutala26@gmail.com>
2025-09-23 23:18:22 +0000

    How about instead can you simply open a mailto: sai.gutala@gmail.com gma

--- src/components/connect-button.tsx
+++ src/components/connect-button.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-import { useState } from 'react';
 import { Button, type ButtonProps } from '@/components/ui/button';
-import { ConnectModal } from '@/components/connect-modal';
 
 interface ConnectButtonProps extends Omit<ButtonProps, 'onClick'> {
   children: React.ReactNode;
 }
 
 export function ConnectButton({ children, ...props }: ConnectButtonProps) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleConnectClick = () => {
+    window.location.href = "mailto:sai.gutala@gmail.com";
+  };
 
   return (
-    <>
-      <Button onClick={() => setIsModalOpen(true)} {...props}>
-        {children}
-      </Button>
-      <ConnectModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
-    </>
+    <Button onClick={handleConnectClick} {...props}>
+      {children}
+    </Button>
   );
 }
