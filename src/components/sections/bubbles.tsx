4d440cd6c0a0d08bf2b38836fb8277940ef1b4ea
Sri Gutala <sri.gutala26@gmail.com>
2025-09-24 02:43:38 +0000

    Need more bubbles floating around on the screen

--- src/components/sections/bubbles.tsx
+++ src/components/sections/bubbles.tsx
@@ -15,7 +15,7 @@ const Bubbles = () => {
       return;
     }
 
-    let bubbleCount = 25;
+    let bubbleCount = 40;
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
